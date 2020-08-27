9fb0cc478144ff56879afeceec5b01fefca8f2f2
Chia Niu <15838622483@163.com>
2020-08-27 10:48:16 +0800

    #2.27

--- src/Page/Home.vue
+++ src/Page/Home.vue
@@ -1,14 +1,18 @@
 <template>
     <div>
         <Menu />
+        <ListData />
     </div>
 </template>
 
 <script>
 import Menu from '../components/Menu'
+import ListData from '../components/ListData'
+
 export default {
     components:{
-        Menu
+        Menu,
+        ListData
     }
 }
 </script>
